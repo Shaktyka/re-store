c67f2e5b8a78b7de11fcc3d970e0f7b6dc1f1022
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-06-06 23:31:45 +0500

    Добавляет компонент App

---
+++ src/components/app/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import ErrorBoundary from '../error-boundary/';
+// import ErrorIndicator from '../error-indicator/';
+
+// import bookStoreService from '../../services/bookstore-service';
+
+import './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  }
+}
+
+export default App;
