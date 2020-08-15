a46ccb772e6e7f6748256c779b3685e5e9f7cd2a
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-08-15 21:59:17 +0500

    Добавляет обработку ошибки при загрузке списка книг

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { connect } from 'react-redux'; // чтобы подключиться к редакс-стору
 import withBookstoreService from '../hoc';
-import { booksLoaded, booksRequested } from '../../actions/';
+import { booksLoaded, booksRequested, booksError } from '../../actions/';
 import { compose } from '../../utils/';
 
 import BookListItem from '../book-list-item/';
 import Spinner from '../spinner/';
+import ErrorIndicator from '../error-indicator/';
 
 import './book-list.css';
 
@@ -15,20 +16,27 @@ class BookList extends React.Component {
 
   componentDidMount() {
     // получаем данные
-    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested, booksError } = this.props;
     booksRequested();
     bookstoreService.getBooks()
       .then((data) => {
         // передаём данные в стор
         booksLoaded(data);
+      })
+      .catch((error) => {
+        booksError(error);
       });
   };
 
   render() {
-  	const { books, loading } = this.props;
+  	const { books, loading, error } = this.props;
 
     if (loading) {
       return <Spinner />
+    }
+
+    if (error) {
+      return <ErrorIndicator />;
     }
 
     return (
@@ -51,13 +59,14 @@ class BookList extends React.Component {
   };
 };
 
-const mapStateToProps = ({ books, loading }) => {
-  return { books, loading };
+const mapStateToProps = ({ books, loading, error }) => {
+  return { books, loading, error };
 };
 
 const mapDispatchToProps = {
   booksLoaded,
   booksRequested,
+  booksError,
 };
 
 export default compose(
